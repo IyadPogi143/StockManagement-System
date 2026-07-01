1b02644abec38018bac6e1adff3c3a5c0dc72b8b
LAB22_USER ON WS13 <L22X16W13@LabHCI.net>
2026-07-01 16:57:25 +0800

    Added backend authentication setup

---
+++ backend/src/test/java/edu/cit/Macopia/StockManagement_System/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.Macopia.StockManagement_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
